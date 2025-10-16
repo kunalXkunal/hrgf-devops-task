5f58073aa254e133b4d63973c06ac11ba3ef86fa
kunalxkunal <kunalp7500@gmail.com>
2025-10-16 15:53:45 +0530

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello HRGF," \
-    "I am Kunal Phale " \
-    "This application was deployed on Azure Kubernetes Service (AKS) using Terraform, Helm, GitHub Actions,"
+    return "Hello HRGF,\nI am Kunal Phale.\nThis application was deployed on Azure Kubernetes Service (AKS) using Terraform, Helm, and GitHub Actions."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
